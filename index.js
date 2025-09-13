b00df30d99913a00cec489ce7a2a62b593b5f922
Ahmed Miras P <your.email@example.com>
2025-09-13 15:10:52 +0530

    DB and connecting to Express

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
 const { resolve } = require('path');
 
-const app = express();
-const port = 3010;
+dotenv.config();
 
+
+const app = express();
+
+app.use(express.json());
+const port = process.env.PORT || 3000;
+const mongoURI = process.env.MONGO_URI;
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.log(err));
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
